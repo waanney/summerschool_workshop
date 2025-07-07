b1440615d027e1dd6dd5e7b66cfedc5a4b815663
waanney <work.buiminhquan@gmail.com>
2025-07-07 08:49:06 +0700

    feat: create prompt by agent via adalflow

--- src/utils/call_files.py
+++
@@ -1,6 +0,0 @@
-from utils.basetools.faq_tool import faq_tool
-from utils.basetools.calculator_tool import calculate_expression, basic_math, trigonometry, logarithm, calculator_memory
-from data.prompts.admission_prompt import ADMISSION_PROMPT
-from llm.base import AgentClient
-from data.cache.redis_cache import ShortTermMemory
-from handlers.ui_handlers import create_chat_handlers 
